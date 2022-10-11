d3865eebfe186068b35a67d0c3ca980f3c8aeb04
Omkar Lonkar <omkarlonkar@umail.ucsb.edu>
2022-10-11 13:34:18 -0700

    Passes all tests

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from omkarlonkar!";
+        return "Greetings from Spring Boot!";
     }
     
 }
